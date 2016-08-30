76197af8797f566920e9b5407166acc46abdffdf
ramazanaslandag <ramazanaslandag@gmail.com>
2016-08-30 22:20:59 +0300

    hatalar düzeltildi

--- SayiTahmin/Program.cs
+++ SayiTahmin/Program.cs
@@ -15,9 +15,8 @@ namespace SayiTahmin
             string istek = "";
             do
             {
-                Console.Clear();//önceki bilgileri sil
-                int uretilenSayi = gen.Next(101);//pc sayı üretsin
-                // Console.WriteLine(uretilenSayi);
+                Console.Clear();
+                int uretilenSayi = gen.Next(101);
                 Console.Write("Tahmininiz:");
                 int kullaniciTahmini = int.Parse(Console.ReadLine());
                 int deneme = 1;
@@ -34,24 +33,13 @@ namespace SayiTahmin
                         Console.Write("Daha büyük bir sayı girişi yapınız:");
                         kullaniciTahmini = int.Parse(Console.ReadLine());
                     }
-                    //sayıların eşitliği döngünün sonlanmasını sağlar bu nedenle bir else bloğuyla bunun kontrol edilmesine gerek yok
+                   
                 }
                 Console.WriteLine("Tebrikler, {1} denemede doğruyu buldunuz sayı {0} olarak üretilmişti.", uretilenSayi, deneme);
                 Console.Write("Yeniden oynamak ister misiniz? (e/h):");
                 istek = Console.ReadLine();
             } while (istek.ToLower()=="e");
-           // Console.ReadLine();
-
-
-            //challenge: 
-            /*
-             ogs:30
-             banka hesabı:50
-             geçiş ücreti=3
-             * ogs de 20nin altına düştüğüm zaman bankada varsa 20 br ogs ye aktarılsın. yoksa, kalanın tamamı ogs ye aktarılsın.
-             * kaç geçiş yaptım, bankada kalan, ogs de kalan
-             */
-            //Console.Beep();
+          
         }
     }
 }
